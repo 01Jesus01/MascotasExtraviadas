d79182ba3f9bf479d21b06e82bbb1fdb28577184
AxelReyes <AxelReyes619@gmail.com>
2023-11-28 23:34:49 -0600

    Actualizacion

---
+++ js/mascota_encontrada.js
@@ -0,0 +1,10 @@
+
+function mostrarMensaje() {
+    // Muestra el mensaje utilizando el método alert
+    alert('Muchas gracias por avisarnos, mantente en contacto con el dueño.');
+
+    // Reproduce un sonido (cambia 'ruta_del_sonido.mp3' por la ubicación de tu archivo de sonido)
+    var audio = new Audio('perroladrando.mp3');
+    audio.play();
+}
+
